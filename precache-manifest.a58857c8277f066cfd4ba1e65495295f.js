2e975a87bf47a7178e94ded696b3bd93afebf417
MayuranSundar <61875478+MayuranSundar@users.noreply.github.com>
2023-03-08 09:43:27 +0000

    Updates

---
+++ precache-manifest.a58857c8277f066cfd4ba1e65495295f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30629ca51dac063ba57c",
+    "url": "/Meme-Generator/static/css/main.e688c0f3.chunk.css"
+  },
+  {
+    "revision": "30629ca51dac063ba57c",
+    "url": "/Meme-Generator/static/js/main.30629ca5.chunk.js"
+  },
+  {
+    "revision": "78033eb50f50ee4219a3",
+    "url": "/Meme-Generator/static/js/1.78033eb5.chunk.js"
+  },
+  {
+    "revision": "1745a5190e70ee1e962f",
+    "url": "/Meme-Generator/static/js/runtime~main.1745a519.js"
+  },
+  {
+    "revision": "f2f0e0632634fee3cd2997aa548bbb2e",
+    "url": "/Meme-Generator/static/media/logo.f2f0e063.png"
+  },
+  {
+    "revision": "01023c5d11e3eb450b525739cd30618f",
+    "url": "/Meme-Generator/index.html"
+  }
+];
